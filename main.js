f62e9505e5a80451d5f7e69826a4ff79a4c88a38
vikram-mahto <vikram.mahto@coolbootsmedia.com>
2024-02-14 00:52:08 +0530

    Added resume preview container inside modal

--- main.js
+++ main.js
@@ -1,41 +1,45 @@
-var pageWidth, pageHeight;
+$(document).on("shown.bs.modal", function () {
+  $(".modal-content").css("min-height", "842px");
+  var pageWidth, pageHeight;
 
-var basePage = {
-  width: 763,
-  height: 600,
-  scale: 1,
-  scaleX: 1,
-  scaleY: 1,
-};
+  var basePage = {
+    width: 763,
+    height: 600,
+    scale: 1,
+    scaleX: 1,
+    scaleY: 1,
+  };
 
-$(function () {
-  var $page = $(".page_content");
+  $(function () {
+    var $page = $(".page_content");
 
-  getPageSize();
-  scalePages($page, pageWidth, pageHeight);
+    getPageSize();
+    scalePages($page, pageWidth, pageHeight);
 
-  //using underscore to delay resize method till finished resizing window
-  $(window).resize(
-    _.debounce(function () {
-      getPageSize();
-      scalePages($page, pageWidth, pageHeight);
-    }, 150)
-  );
+    //using underscore to delay resize method till finished resizing window
+    $(window).resize(
+      _.debounce(function () {
+        getPageSize();
+        scalePages($page, pageWidth, pageHeight);
+      }, 150)
+    );
 
-  function getPageSize() {
-    pageHeight = $("#container").height();
-    pageWidth = $("#container").width();
-  }
+    function getPageSize() {
+      pageHeight = $("#container").height();
+      pageWidth = $("#container").width();
+    }
 
-  function scalePages(page, maxWidth, maxHeight) {
-    var scaleX = 1,
-      scaleY = 1;
-    scaleX = maxWidth / basePage.width;
-    scaleY = maxHeight / basePage.height;
-    basePage.scaleX = scaleX;
-    basePage.scaleY = scaleY;
-    basePage.scale = scaleX > scaleY ? scaleY : scaleX;
-    var scaleValue = basePage.scale - 0.03;   
-    page.attr("style", "-webkit-transform:scale(" + scaleValue + ");");
-  }
+    function scalePages(page, maxWidth, maxHeight) {
+      var scaleX = 1,
+        scaleY = 1;
+      scaleX = maxWidth / basePage.width;
+      scaleY = maxHeight / basePage.height;
+      basePage.scaleX = scaleX;
+      basePage.scaleY = scaleY;
+      // basePage.scale = scaleX > scaleY ? scaleY : scaleX;
+      basePage.scale = scaleX;
+      var scaleValue = basePage.scale - 0.04;
+      page.attr("style", "-webkit-transform:scale(" + scaleValue + ");");
+    }
+  });
 });
