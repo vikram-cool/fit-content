a3877da26f320ca707694d850793e51908204b11
vikram-mahto <vikram.mahto@coolbootsmedia.com>
2024-02-14 11:43:55 +0530

    Dynamic page break test

---
+++ page-break/page-break.js
@@ -0,0 +1,37 @@
+const pageWidth = 320;
+const content = document.getElementById("content");
+const totalWidth = content.scrollWidth;
+const totalPages = totalWidth / pageWidth;
+console.log("totalPages", totalPages);
+
+let contentVisible = true;
+const button = document.getElementById("btn-content");
+const buttonText = document.getElementById("btn-content-text");
+const showHideContent = () => {
+  contentVisible = !contentVisible;
+  content.style.display = contentVisible ? "block" : "none";
+  buttonText.innerText = contentVisible ? "Hide" : "Show";
+};
+button.addEventListener("click", showHideContent);
+
+const html = content.innerHTML;
+const container = document.getElementById("container");
+// console.log('content', content);
+for (let p = 0; p < totalPages; p++) {
+  const page = document.createElement("div");
+  page.innerHTML = html;
+  page.className = "page";
+  page.style.cssText = `
+    width: ${totalWidth}px;
+    transform: translateX(-${p * pageWidth}px);
+  `;
+  const pageClip = document.createElement("div");
+  pageClip.className = "page-clip";
+  pageClip.appendChild(page);
+  const pageWrapper = document.createElement("div");
+  pageWrapper.className = "page-wrapper";
+  pageWrapper.appendChild(pageClip);
+  container.appendChild(pageWrapper);
+}
+
+showHideContent();
